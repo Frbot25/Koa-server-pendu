4c5cdb69dc3645255f5013be060d7621a562bcca
Frederic Botella <frederic.botella@gmail.com>
2022-06-06 17:38:00 +0200

    npm install koa-bodyparser

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
 const router = require("./app/router");
 const app = new Koa();
+const cors = require('@koa/cors');
+const bodyParser = require('koa-bodyparser');
 
 const port = 3000
+app.use(cors());
+app.use(bodyParser());
 //Routes 
 app.use(router.routes()).use(router.allowedMethods());
 
